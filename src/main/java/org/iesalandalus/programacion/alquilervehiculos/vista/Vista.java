61270a817140aa0314ae6cce97b712b86759e64a
Irishongki <cpirene2000@gmail.com>
2023-02-19 22:11:02 +0100

    Clase Vista parcialmente finalizada

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Vista.java
@@ -0,0 +1,165 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista;
+
+import java.util.Iterator;
+
+import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+
+public class Vista {
+
+	private Controlador controlador;
+
+	public void setControlador(Controlador controlador) {
+		if (controlador == null) {
+			throw new NullPointerException("El controlador no puede ser nulo");
+		}
+		this.controlador = controlador;
+	}
+
+	public void comenzar() {
+		do {
+			Consola.mostrarMenu();
+			ejecutar(Consola.elegirOpcion());
+		} while (Consola.elegirOpcion() == null);
+	}
+
+	public void terminar() {
+		System.out.print("Adioss!!");
+	}
+
+	private void ejecutar(Opcion opcion) {
+		switch (opcion.ordinal()) {
+		case 0:
+			terminar();
+			break;
+		case 1:
+			insertarCliente();
+			break;
+		case 2:
+			insertarTurismo();
+			break;
+		case 3:
+			insertarAlquiler();
+			break;
+		case 4:
+			buscarCliente();
+			break;
+		case 5:
+			buscarTurismo();
+			break;
+		case 6:
+			buscarAlquiler();
+			break;
+		case 7:
+			modificarCliente();
+			break;
+		case 8:
+			devolverAlquiler();
+			break;
+		case 9:
+			borrarCliente();
+			break;
+		case 10:
+			borrarTurismo();
+			break;
+		case 11:
+			borrarAlquiler();
+			break;
+		case 12:
+			listarClientes();
+			break;
+		case 13:
+			listarTurismos();
+			break;
+		case 14:
+			listarAlquileres();
+			break;
+		case 15:
+			listarAlquileresCliente();
+			break;
+		case 16:
+			listarAlquileresTurismo();
+			break;
+		}
+	}
+
+	private void insertarCliente() {
+		Consola.mostrarCabecera("Estamos en la opción --> 1-Insertar Cliente");
+		controlador.insertar(Consola.leerCliente());
+	}
+
+	private void insertarTurismo() {
+		Consola.mostrarCabecera("Estamos en la opción --> 2-Insertar Turismo");
+		controlador.insertar(Consola.leerTurismo());
+	}
+
+	private void insertarAlquiler() {
+		Consola.mostrarCabecera("Estamos en la opción --> 3-Insertar Alquiler");
+		controlador.insertar(Consola.leerAlquiler());
+	}
+
+	private void buscarCliente() {
+		Consola.mostrarCabecera("Estamos en la opción --> 4-Buscar Cliente");
+		controlador.buscar(Consola.leerClienteDni());
+	}
+
+	private void buscarTurismo() {
+		Consola.mostrarCabecera("Estamos en la opción --> 5-Buscar Turismo");
+		controlador.buscar(Consola.leerTurismoMatricula());
+	}
+
+	private void buscarAlquiler() {
+		Consola.mostrarCabecera("Estamos en la opción --> 6-Buscar Alquiler");
+		controlador.buscar(Consola.leerAlquiler());
+	}
+
+	private void modificarCliente() {
+		Consola.mostrarCabecera("Estamos en la opción --> 7-Modificar Cliente");
+		controlador.modificar(Consola.leerClienteDni(), Consola.leerNombre(), Consola.leerTelefono());
+	}
+
+	private void devolverAlquiler() {
+		Consola.mostrarCabecera("Estamos en la opción --> 8-Devolver Alquiler");
+		controlador.devolver(Consola.leerAlquiler(), Consola.leerFechaDevolucion());
+	}
+
+	private void borrarCliente() {
+		Consola.mostrarCabecera("Estamos en la opción --> 9-Borrar Cliente");
+		controlador.borrar(Consola.leerClienteDni());
+	}
+
+	private void borrarTurismo() {
+		Consola.mostrarCabecera("Estamos en la opción --> 10-Borrar Turismo");
+		controlador.borrar(Consola.leerTurismoMatricula());
+	}
+
+	private void borrarAlquiler() {
+		Consola.mostrarCabecera("Estamos en la opción --> 11-Borrar Alquiler");
+		controlador.borrar(Consola.leerAlquiler());
+	}
+
+	private void listarClientes() {
+		Consola.mostrarCabecera("Estamos en la opción --> 12-Listar Clientes");
+		controlador.getClientes();
+	}
+
+	private void listarTurismos() {
+		Consola.mostrarCabecera("Estamos en la opción --> 13-Listar Turismos");
+		controlador.getTurismos();
+	}
+
+	private void listarAlquileres() {
+		Consola.mostrarCabecera("Estamos en la opción --> 14-Listar Alquileres");
+		controlador.getAlquileres();
+	}
+
+	private void listarAlquileresCliente() {
+		Consola.mostrarCabecera("Estamos en la opción --> 15-Listar los Alquileres de un Cliente");
+		controlador.getAlquileres(Consola.leerClienteDni());
+	}
+
+	private void listarAlquileresTurismo() {
+		Consola.mostrarCabecera("Estamos en la opción --> 16-Listar los Alquileres de un Turismo");
+		controlador.getAlquileres(Consola.leerTurismoMatricula());
+	}
+
+}
